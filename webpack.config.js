09269a80b0f7b19b10939ede4d30bd12e01e4ef3
colodoo <lodog1@qq.com>
2018-08-22 09:52:54 +0800

    热部署

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,24 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    entry: {
-        tup: __dirname + '\\src\\tup.js',
-    },
-    output: {
-        path: __dirname + '\\dist',
-        filename: '[name].js',
-        library: 'Tup',
-        libraryTarget: 'umd',
-    },
-    devServer: {
-        contentBase: "./examples",  //以examples为根目录提供文件
-        colors: true,
-        historyApiFallback: true,
-        inline: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJsPlugin()
+  entry: {
+    tup: __dirname + '\\src\\tup.js',
+  },
+  output: {
+    path: __dirname + '\\dist',
+    filename: '[name].js',
+    library: 'Tup',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 }
